2020623c8671307c71248775b618988444979e21
Daniyar Bashirov <tehnikdaniyar@gmail.com>
2023-02-28 23:44:22 +0600

    working on header

--- src/js/modules/actions.js
+++ src/js/modules/actions.js
@@ -5,6 +5,7 @@ export const actions = () => {
 
 		function documentActions(e) {
 			const targetElement = e.target;
+			console.log(targetElement);
 
 			if (targetElement.classList.contains('menu__arrow')) {
 				targetElement.closest('.menu__item').classList.toggle('_hover')
@@ -20,7 +21,12 @@ export const actions = () => {
 			else
 				if (!targetElement.closest('.search-form') && document.querySelector('.search-form._active')) {
 					document.querySelector('.search-form').classList.remove('_active');
-				}
+				};
+
+			if (targetElement.closest('.icon-menu')) {
+				console.log('burger');
+				document.querySelector('.menu__body').classList.toggle('_active');
+			}
 
 		};
 	};
